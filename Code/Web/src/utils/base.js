b5ddf4ccdd45390c7371d604c634804784cdf82f
datnd299 <35355001+datnd299@users.noreply.github.com>
2020-03-30 14:50:56 +0700

    Update base.js

--- Code/Web/src/utils/base.js
+++ Code/Web/src/utils/base.js
@@ -1,4 +1,4 @@
 module.exports={
-    // BASE_API:'http://103.35.64.5:8086/api/v1/',
-    BASE_API:'http://localhost:8086/api/v1/'
+     BASE_API:'http://103.35.64.5:8086/api/v1/',
+    // /BASE_API:'http://localhost:8086/api/v1/'
 }
